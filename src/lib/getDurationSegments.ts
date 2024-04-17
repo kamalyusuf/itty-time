7e6fa3704e343328d54bce0927f01e505d2c8786
Kevin Whitley <krwhitley@gmail.com>
2024-04-17 13:12:05 -0500

    cleanup

--- src/lib/getDurationSegments.ts
+++ src/lib/getDurationSegments.ts
@@ -3,21 +3,8 @@ type DurationSegment = [
   unit: string
 ]
 
-// FUNCTION: get { value, unit } from a duration string
-// export const getDurationSegments = (duration: string): DurationSegment => {
-//   const match = duration.toLowerCase().match(/^(?<v>[+-\d.]+)?\s?(?<u>\w{2,}?)s?$/)
-//   // @ts-ignore
-//   const { v, u } = match.groups
-//   const value = +(v === undefined ? 1 : v)
-
-//   return [value, u]
-// }
-
 export const getDurationSegments = (duration: string): DurationSegment => {
   const match = duration.toLowerCase().match(/^([+-\d.]+)?\s?(\w{2,}?)s?$/) || []
-  // @ts-ignore
-  // const [] = match.groups
-  // const value = +(v === undefined ? 1 : v)
 
   return [+match[1], match[2]]
 }
