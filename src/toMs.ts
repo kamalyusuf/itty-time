2a8781883b889482238151c8206ca56ad4b591b1
Kevin Whitley <krwhitley@gmail.com>
2024-04-17 13:48:58 -0500

    more code golf

--- src/toMs.ts
+++ src/toMs.ts
@@ -3,13 +3,15 @@ import { units } from './lib/units'
 
 // FUNCTION: get number of seconds from a duration string
 export const toMs = (duration: string | number, ms = 0): number =>
-  isNaN(+duration)
-  ? duration
-      // @ts-ignore
-      .split(/,?\s*and\s*|,\s*/)
-      .reduce((acc: any, d: any) => {
-        const [value, unit] = getDurationSegments(d)
-        return acc + (units[unit] ? units[unit] * value * 1000 : value)
-      }, ms)
-  : duration
+  Number(duration) ||
+  duration
+    // @ts-ignore
+    .split(/,?\s*and\s*|,\s*/)
+    .reduce((
+      acc: any,
+      d: any,
+      a: any,
+      b: any,
+      [value, unit] = getDurationSegments(d)
+    ) => acc + (units[unit] ? units[unit] * 1000 : 1) * value, ms)
 
--- src/toSeconds.ts
+++ src/toSeconds.ts
@@ -1,20 +1,4 @@
-// import { getDurationSegments } from './lib/getDurationSegments'
-// import { units } from './lib/units'
-
-// // FUNCTION: get number of seconds from a duration string
-// export const toSeconds = (duration: string, seconds = 0) => {
-//   const durations = duration.split(/,?\s*and\s*|,\s*/)
-//   // const now = Date.now()
-//   // let next = now
-
-//   for (const d of durations) {
-//     const { value, unit } = getDurationSegments(d)
-//     seconds += units[unit] ? units[unit] * value : 0
-//   }
-
-//   return next - now
-// }
-
 import { toMs } from './toMs'
 
-export const toSeconds = (duration: string | number): number => toMs(duration) / 1000
+export const toSeconds = (duration: string | number): number =>
+  toMs(duration) / 1000
