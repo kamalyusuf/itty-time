61a12ee23bf660fa8817d92b2b4ed5384535c1ae
Kevin Whitley <krwhitley@gmail.com>
2024-04-19 00:14:56 -0500

    i lied

--- src/ms.ts
+++ src/ms.ts
@@ -1,13 +1,10 @@
 import { units } from './lib/units'
 
-type Ms = (duration: string | number) => number
-
 // FUNCTION: get number of seconds from a duration string
-export const ms: Ms = (duration: string | number): number => {
+export const ms = (duration: string | number): number => {
   if (+duration) return +duration
-
   // @ts-ignore
-  // const [, value, unit] = duration.match(/^(.+) +(\w\w*?)s?$/) || [] // slower
   const [, value, unit] = duration.match(/^([^ ]+) *(\w\w*?)s?$/) || []
+
   return +value * (units[unit] || 1)
 }
