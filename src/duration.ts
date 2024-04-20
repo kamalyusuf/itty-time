26cfab7c3abb62a7bc5cc35cf4d4fe6006f53156
Kevin Whitley <krwhitley@gmail.com>
2024-04-20 14:07:45 -0500

    working...

--- src/duration.ts
+++ src/duration.ts
@@ -20,20 +20,12 @@ export const duration: DurationType = (
   }: DurationOptions = {},
 ) => {
   let count, result: [string, number][] = []
-  // console.log('calculating duration of', ms)
 
   for (let [unit, value] of Object.entries(units)) {
-    // console.log('comparing ms', ms, 'to unit', unit, 'with value', value)
     if ((ms >= value || unit == 'second') && parts-- > 0) {
-      // if (unit == 'second')
-        // console.log('calculating seconds of', ms)
       ms -= (count = ms / value | 0) * value
       if (!parts || unit == 'second') count += ms / value
       if (count != 1) unit += 's'
-
-      // if (unit == 'second')
-        // console.log('calculated seconds', count)
-
       // @ts-ignore
       result.push(join ? count + ' ' + unit : [unit, count])
       if (!ms) break
