d1ad08f9ea728d8ed37ff865cbce1dd8f35553e7
Kamal <kbomoi@yahoo.com>
2024-07-29 03:26:04 +0100

    types: improve ms function typing for singular/plural units

--- src/ms.ts
+++ src/ms.ts
@@ -1,7 +1,9 @@
 import { units } from './lib/units'
 
+type Unit = 'second' | 'minute' | 'hour' | 'day' | 'week' | 'month' | 'year';
+
 // FUNCTION: get number of seconds from a duration string
-export const ms = (duration: string | number): number => {
+export const ms = (duration: `${number} ${Unit | `${Unit}s`}` | number): number => {
   if (+duration) return +duration
   // @ts-ignore
   const [, value, unit] = duration.match(/^([^ ]+) +(\w\w*?)s?$/) || []
